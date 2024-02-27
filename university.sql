eeb599f0c44c7c44ff5e8cedf94e4eb04e099f6a
linus-rudbeck <linus.rudbeck@distansakademin.se>
2024-02-27 10:44:19 +0100

    Update university.sql

--- university.sql
+++ university.sql
@@ -69,7 +69,10 @@ INSERT INTO student_course_grades (student_id, course_id, grade, start_date, end
 
 
 
-SELECT * FROM student_course_grades;
-
+SELECT s.student_name, c.course_name, scg.grade, u.university_name
+	FROM student_course_grades AS scg
+    INNER JOIN students AS s ON s.student_id = scg.student_id
+    INNER JOIN courses AS c ON c.course_id = scg.course_id
+    INNER JOIN universities AS u ON u.university_id = c.university_id;
 
 
